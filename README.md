289cac5fcb4ef809e361a14bd4a391a58c808f02
Kevin <kevinsetiawan005@gmail.com>
2023-05-28 23:06:58 +0700

    first draft

---
+++ README.md
@@ -0,0 +1,7 @@
+# Best Movies
+
+Best Movies is a web app that list the best movies out there based on user input. So that anyone who wants to share their favorite underrated movie, can recommend it using this site. It's also a way for movie lovers to find movie recommendations from people witj different tastes.
+
+You can access the app here: https://best-movies.vercel.app/
+
+
---
+++ index.js
@@ -0,0 +1,73 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://best-movies-db-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const movieListDB = ref(database, "movieList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const movieListEl = document.getElementById("movie-list")
+
+
+addButtonEl.addEventListener("click", function() { //only updates if the movie written is not in the list
+    let inputValue = inputFieldEl.value
+    let allMovies = movieListEl.innerText.toLowerCase().split('\n')
+    
+    if(allMovies.length>0 && allMovies.includes(inputValue.toLowerCase())) {
+        alert("Already Added!")
+    } 
+    else if (inputValue.trim().length!==0){
+        push(movieListDB, inputValue)
+    }
+    clearInputFieldEl()
+})
+
+onValue(movieListDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearMovieListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendToMovieListEl(currentItem)
+        }    
+    } else {
+        movieListEl.innerHTML = "No titles has been inputted"
+    }
+})
+
+function clearMovieListEl() {
+    movieListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendToMovieListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    // Delete Item from DB
+    newEl.addEventListener("click", function() {
+        navigator.clipboard.writeText(newEl.textContent);
+        // let exactLocationOfItemInDB = ref(database, `movieList/${itemID}`)
+        
+        // remove(exactLocationOfItemInDB)
+    })
+    
+    movieListEl.append(newEl)
+}
